825f722faf3e49ac55df078f1eb63602ecb3756f
Michael <michael.tandoc@ryerson.ca>
2021-09-29 15:04:08 -0400

    Setup React project w/ next.js and prisma

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>Home Page</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
