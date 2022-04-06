612ee51d6a0b6c543b9adee54ee64d0ee8d4f084
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-04-06 15:41:34 -0400

    Edit to Day component

--- components/Calendar/Day.tsx
+++ components/Calendar/Day.tsx
@@ -14,15 +14,16 @@ const Day = (props: Props) => {
   //const [hover, setHover] = useState(props.hover)
 
   let className = "calendar-day"
-  if (!props.currentMonth) {
-    className += " diff-month"
+  if (props.currentMonth) {
+    className += " curr-month"
   }
 
   if (props.date.hasSame(DateTime.now(), "day")) {
     className += " today"
   }
 
-  if (props.selectedDay.hasSame(props.date, "day")) {
+  const isSelected = props.selectedDay.hasSame(props.date, "day")
+  if (isSelected) {
     className += " selected"
   }
 
@@ -34,6 +35,7 @@ const Day = (props: Props) => {
       className={className}
       style={{ gridColumn: props.date.weekday.toString() }}
       onClick={() => props.setSelectedDay(props.date)}
+      hidden={!props.currentMonth}
     >
       <div className="date-label">{props.date.day}</div>
       {props.renderDayContent?.(props.date)}
@@ -42,8 +44,9 @@ const Day = (props: Props) => {
           font-size: 0.8em;
           margin-left: 5%;
         }
-        .calendar-day.diff-month {
+        .calendar-day:not(.curr-month) {
           color: rgb(179, 179, 179);
+          display: none;
         }
         .calendar-day.today {
           background-color: rgb(206, 229, 236) !important;
